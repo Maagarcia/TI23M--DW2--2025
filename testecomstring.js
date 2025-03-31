c951a43bace31db5fb84b56dc6bc5c28c8cd95cf
Maagarcia <153952539+Maagarcia@users.noreply.github.com>
2025-03-31 08:07:37 -0300

    TESTE DE VOLTA AS AULAS

---
+++ testecomstring.js
@@ -0,0 +1,29 @@
+function par(n){
+    console.log(typeof(n))
+    if (n %2 == 1){
+        return false
+    }else{
+        return true
+    }
+}
+
+
+
+function parfodão(x){
+    console.log(id = x % 2 == 1? true: false)
+}
+// x = par(4)
+// console.log(x)
+// console.log(par(21))
+
+// parfodão(1221)
+// parfodão(1222)
+
+function testefodao(x,y){
+    return x * y
+
+}
+
+i =  testefodao(8e-2,6)
+console.log(i)
+
