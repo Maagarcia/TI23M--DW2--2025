cf8117607299ba53103588a9fdb728ab885767ad
Maagarcia <153952539+Maagarcia@users.noreply.github.com>
2025-06-18 16:00:37 -0300

    mostra estatistica dos tiros dinamicamente

--- Projetos-Avulsos/Aim.js
+++ Projetos-Avulsos/Aim.js
@@ -2,16 +2,17 @@ let score = 0
 let codX = []
 let codY = []
 
+let num_testes = 0
+
 function Coordenada(){
     document.getElementById('CoordenadaX').innerHTML = `Coordenada X:${event.clientX}`
     document.getElementById('CoordenadaY').innerHTML = `Coordenada Y:${event.clientY}`
 }
 
-
-
-
 function target(){
-    console.log('teste')
+    num_testes +=1
+    console.log('numteste', num_testes)
+    document.getElementById('target').style.display = 'none'
     const alvo = document.createElement('button')
     const showestatistica = document.getElementById('show-estatisticas')
     showestatistica.style.display = 'none'
@@ -29,10 +30,34 @@ function target(){
     alvo.style.top = `${parseInt(Math.random() * 700)}px`
     alvo.style.left = `${parseInt(Math.random() * 1000)}px`
     setTimeout(()=>{
+        document.getElementById('target').style.display = 'block'
         showestatistica.style.display = 'flex'
         alvo.style.top =` 354px;`
         alvo.style.left = '654px;'
         alert(`tempo esgotado: sua pontuação foi de ${score}!`)
+
+        var coordenadas_marcadas =  document.getElementById('coordenadas-marcadas')
+        var num_testes_container = document.createElement('div')
+        var title_shot = document.createElement('h1')
+        title_shot.className = 'title-shot'
+        title_shot.textContent = `Teste ${num_testes}`
+
+        coordenadas_marcadas.appendChild(title_shot)
+        
+        for(i=0; i < score; i++){
+            codExato = [codX[i],codY[i]]
+            console.log(`tiro ${i+1} foi dado em (${codExato})`)
+            var estatistica_shot = document.createElement('div')
+            estatistica_shot.textContent = (`tiro ${i+1} foi dado em (${codExato})`)
+
+            num_testes_container.appendChild(estatistica_shot)
+            coordenadas_marcadas.appendChild(num_testes_container)
+
+        }
+        
+        
+        codX = []
+        codY=[]
         end_game()
     
     },5000)
