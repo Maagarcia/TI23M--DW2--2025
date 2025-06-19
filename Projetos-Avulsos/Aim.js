c42aa2ee0291bae027cc9e9e02fa56aac09b14f1
Maagarcia <153952539+Maagarcia@users.noreply.github.com>
2025-06-19 01:04:24 -0300

    treino de valorant criado

--- Projetos-Avulsos/Aim.js
+++ Projetos-Avulsos/Aim.js
@@ -1,3 +1,5 @@
+
+
 let score = 0
 let codX = []
 let codY = []
@@ -9,25 +11,54 @@ function Coordenada(){
     document.getElementById('CoordenadaY').innerHTML = `Coordenada Y:${event.clientY}`
 }
 
-function contagem(){
-    let tempo = 3
-
-    if(tempo > 0){
-        setInterval(()=>{
-            document.getElementById('contador').textContent = `${tempo}...`
+function contagem(nome){
+    if(nome.name === "treino-normal"){
+        document.getElementById('target').style.display = 'none'
+        document.getElementById('target_valorant').style.display = 'none'
+        let tempo = 3
+        console.log(tempo)
+        const contagem_regressiva = setInterval(()=>{
+            let title_contador = document.getElementById('contador')
+            title_contador.style.display = 'flex'
+            title_contador.textContent = `${tempo}...`
             tempo -=1
+            console.log(tempo)
+            if(tempo < 0){
+                clearInterval(contagem_regressiva)
+                title_contador.style.display = 'none'
+                target()
+            }
         },1000)
+        tempo = 3
+        console.log('teste')
     }
-    else{
-        target()
+    if(nome.name === "treino-valorant"){
+        document.getElementById('target').style.display = 'none'
+        document.getElementById('target_valorant').style.display = 'none'
+        let tempo = 3
+        console.log(tempo)
+        const contagem_regressiva = setInterval(()=>{
+            let title_contador = document.getElementById('contador')
+            title_contador.style.display = 'flex'
+            title_contador.textContent = `${tempo}...`
+            tempo -=1
+            console.log(tempo)
+            if(tempo < 0){
+                clearInterval(contagem_regressiva)
+                title_contador.style.display = 'none'
+                treino_valorant()
+            }
+        },1000)
+        tempo = 3
+        console.log('teste')
     }
-    console.log('teste')
 }
+
+
 
 function target(){
     num_testes +=1
     console.log('numteste', num_testes)
-    document.getElementById('target').style.display = 'none'
     const alvo = document.createElement('button')
     const showestatistica = document.getElementById('show-estatisticas')
     showestatistica.style.display = 'none'
@@ -50,13 +81,13 @@ function target(){
         alvo.style.top =` 354px;`
         alvo.style.left = '654px;'
         alert(`tempo esgotado: sua pontuação foi de ${score}!`)
-
+        
         var coordenadas_marcadas =  document.getElementById('coordenadas-marcadas')
         var num_testes_container = document.createElement('div')
         var title_shot = document.createElement('h1')
         title_shot.className = 'title-shot'
         title_shot.textContent = `Teste ${num_testes}`
-
+        
         coordenadas_marcadas.appendChild(title_shot)
         
         for(i=0; i < score; i++){
@@ -64,18 +95,18 @@ function target(){
             console.log(`tiro ${i+1} foi dado em (${codExato})`)
             var estatistica_shot = document.createElement('div')
             estatistica_shot.textContent = (`tiro ${i+1} foi dado em (${codExato})`)
-
+            
             num_testes_container.appendChild(estatistica_shot)
             coordenadas_marcadas.appendChild(num_testes_container)
-
+            
         }
         
         
         codX = []
         codY=[]
         end_game()
-    
-    },500)
+        
+    },10000)
     
 }
 
@@ -86,10 +117,19 @@ function game(){
     
     new_mosca.style.top = `${parseInt(Math.random() * 600)}px`
     new_mosca.style.left = `${parseInt(Math.random() * 1000)}px`
-
+    
     codX.push(event.clientX)
     codY.push(event.clientY)
     
+}
+
+function valorant_alvo(){
+    score +=1
+    // num_alvos +=1
+    document.getElementById('mosca').remove()
+    console.log('esse é o numero de alvos atirados',score)
+    document.getElementById('score').textContent = `Pontuação: ${score}`
+
 }
 
 function end_game(){
@@ -102,9 +142,49 @@ function end_game(){
 
 function show_estatistica(){
     document.getElementById('estatisticas').style.display = 'block'
+    
 
 }
 
 function fecha_estatistica(){
     document.getElementById('estatisticas').style.display = 'none'
+}
+
+function treino_valorant(){
+    let num_alvos = 0
+    num_testes +=1
+    console.log('numteste', num_testes)
+    const game_valorant = setInterval(() => {
+        num_alvos +=1
+        const alvo = document.createElement('button')
+        const showestatistica = document.getElementById('show-estatisticas')
+        showestatistica.style.display = 'none'
+        document.getElementById('estatisticas').style.display = 'none'
+        
+        
+        alvo.setAttribute('id',true)
+        alvo.id = 'mosca'
+        alvo.setAttribute('class',true)
+        alvo.classList = 'ph'
+        alvo.setAttribute('onclick', 'valorant_alvo()')
+        const body_container = document.getElementById("body-container")
+        
+        body_container.appendChild(alvo)
+        alvo.style.top = `${parseInt(Math.random() * 600)}px`
+        alvo.style.left = `${parseInt(Math.random() * 1000)}px`
+
+
+        setTimeout(() => {
+                console.log('esse é o numero de alvos passados',num_alvos)
+                alvo.remove()
+            
+                if(num_alvos >= 10){
+                    clearInterval(game_valorant)
+                     document.getElementById('target').style.display = 'block'
+                     document.getElementById('target_valorant').style.display = 'block'
+                }
+        }, 1000);
+    }, 1500);
+
+    // for(i=)
 }
