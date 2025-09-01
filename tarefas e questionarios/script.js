319d79247a7c81f39a93da2d1eaba8ae23e183fb
Maagarcia <153952539+Maagarcia@users.noreply.github.com>
2025-09-01 20:27:21 -0300

    sdcsd

--- tarefas e questionarios/script.js
+++ tarefas e questionarios/script.js
@@ -3,3 +3,9 @@ function boasVindas(){
     let x = document.getElementById("nome-completo").value
     alert('Seja bem vindo ' + x);	
 }
+
+const lista = ['maça','banana','mamao']
+
+lista.map(intem =>{console.log(intem)})
+
+console.log(lista)
