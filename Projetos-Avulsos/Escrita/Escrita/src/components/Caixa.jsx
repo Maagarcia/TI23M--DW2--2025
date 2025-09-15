2f801e95dfd7327f6d4d6142ffa9e12fd87bb051
Maagarcia <153952539+Maagarcia@users.noreply.github.com>
2025-09-15 08:50:15 -0300

    apresenta percentual de acertos

--- Projetos-Avulsos/Escrita/Escrita/src/components/Caixa.jsx
+++ Projetos-Avulsos/Escrita/Escrita/src/components/Caixa.jsx
@@ -1,22 +1,46 @@
 import { useState } from "react"
 
+const palavrasArray = ['hello','hi','popsicle']
+
+let palavraDoArray = palavrasArray[Math.floor((Math.random())*palavrasArray.length)]
+
+
+
 export default function Caixa(){
+    const [numerodepalavras, setNumeroDePalavras] = useState(0)
+
+    const [percentagem, setPercentagem] = useState(0)
+    
     const [palavra,setPalavra] = useState('')
-
+    
     const [count,setCount] = useState(0)
-
+    
     const handleSubmit = (e)=>{
         e.preventDefault()
         setPalavra('')
         console.log('log')
+    
+        
     }
-
+    
     const handleChange = (e)=>{
         setPalavra(e.target.value)
-
-        if(palavra == 'labubu'){
-            setCount(count+1)
-            console.log('sncsk')
+        
+        if((e.target.value).length == (palavraDoArray).length){
+            if(e.target.value == palavraDoArray){
+                setCount(count+1)
+                palavraDoArray = palavrasArray[Math.floor((Math.random())*palavrasArray.length)]
+                setPalavra('')
+                console.log('sncsk')
+                setNumeroDePalavras(numerodepalavras+1)
+                setPercentagem(count/numerodepalavras)
+                
+            }else{
+                palavraDoArray = palavrasArray[Math.floor((Math.random())*palavrasArray.length)]
+                setNumeroDePalavras(numerodepalavras+1)
+                setPalavra('')
+                setPercentagem(count/numerodepalavras)
+            }
         }
     }
     return(
@@ -28,6 +52,9 @@ export default function Caixa(){
 
         </form>
         <p>{count}</p>
+        <p>{palavraDoArray}</p>
+        <p>Porcentagem de acerto:{(percentagem * 100).toFixed(1)}%</p>
+        <p>Numero de palavras:{numerodepalavras}</p>
         </>
         
     )
