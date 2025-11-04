d5adb50c741e33f2b5df7a155bd35f480c4d51e4
Maagarcia <153952539+Maagarcia@users.noreply.github.com>
2025-11-04 06:30:58 -0300

    mudança na navbar acho q vou ter que refazer

--- Projetos-Avulsos/Login/Routes/src/Navbar.jsx
+++ Projetos-Avulsos/Login/Routes/src/Navbar.jsx
@@ -7,22 +7,46 @@ import { Link } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
 export const Navbar = () => {
 
-  const[tamanho, setTamanho] = useState(null)
+  const[tamanho, setTamanho] = useState(true)
   
   const openSide = () => {
-    
+    setTamanho(!tamanho) 
   }
 
 
   return (
     <>
     <header className={styles.cabecalho}>
-    <Link to='/home' className={styles.hyperlinks}>Home</Link>
+    {/* <Link to='/home' className={styles.hyperlinks}>Home</Link>
+    <Link to='aboutme' className={styles.hyperlinks}>Quem sou eu?</Link>
+    <Link to='projects' className={styles.hyperlinks}>Projetos</Link>
+    <Link to='estagio' className={styles.hyperlinks}>Estágio</Link>
+    <Link to= 'ela' className={styles.hyperlinks}>Por quem você segue</Link> */}
+
+    { tamanho ? (
+      <>
+      <Link to='/home' className={styles.hyperlinks}>Home</Link>
     <Link to='aboutme' className={styles.hyperlinks}>Quem sou eu?</Link>
     <Link to='projects' className={styles.hyperlinks}>Projetos</Link>
     <Link to='estagio' className={styles.hyperlinks}>Estágio</Link>
     <Link to= 'ela' className={styles.hyperlinks}>Por quem você segue</Link>
+      </>)
+
+
+    : ( <>
+        <div className={ { tamanho } ? styles.sideopen : styles.sideclosed } >
+          <li><Link to='/home' className={styles.hyperlinks}>Home</Link></li>
+          <li><Link to='aboutme' className={styles.hyperlinks}>Quem sou eu?</Link></li>
+          <li><Link to='projects' className={styles.hyperlinks}>Projetos</Link></li>
+          <li><Link to='estagio' className={styles.hyperlinks}>Estágio</Link></li>
+          <li><Link to= 'ela' className={styles.hyperlinks}>Por quem você segue</Link></li>
+
+
+        </div>
+    
+    </>)}
     <button className={styles.bar} onClick={openSide}>dc</button>
+
     </header>
     <Outlet/>
       
